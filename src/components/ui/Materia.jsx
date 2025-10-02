3da6457e5919f662f612304e388b4b8568d57eca
Matheus <matheus300nalves@gmail.com>
2025-10-02 17:27:05 -0300

    Estruturando as anotações e matérias

---
+++ src/components/ui/Materia.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom"
+
+export default function Materia() {
+    var materia = {
+        nome: "Matemática",
+        serie: "1o Ano Ensino Médio"
+    }
+
+    return (
+        <article className="materia">
+            <div className="info">
+                <h3>{materia.nome}</h3>
+                <p>{materia.serie}</p>
+            </div>
+            <Link to="/materia">
+                <div className="link-materia">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+            </Link>
+        </article>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,26 @@
+import { Link } from "react-router-dom"
 import Header from "../components/layout/Header"
+import Materia from "../components/ui/Materia"
+import Anotacao from "../components/ui/Anotacao"
 
 export default function Home() {
     return (
         <>
             <Header />
+            <main>
+                <section id="materias" className="estudos-container">
+                    <div className="container-title">
+                        <h2>Suas Máterias</h2>
+                        <Link to="/add"><img src="/btnAdd.svg" alt="Adicionar Matéria"/></Link>
+                    </div>
+                </section>
+                <section id="anotacoes" className="estudos-container">
+                    <div className="container-title">
+                        <h2>Suas Anotações</h2>
+                        <Link to="/add"><img src="/btnAdd.svg" alt="Adicionar Anotação"/></Link>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
