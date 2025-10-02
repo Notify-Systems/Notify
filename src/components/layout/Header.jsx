ac97126d4ac25bff4566f2b2db3868a6e3c056f0
pedro <pedrosantaella61@gmail.com>
2025-10-01 21:46:47 -0300

    Estruturando barra de pesquisa

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,14 +2,20 @@ import { Link } from "react-router-dom"
 
 export default function Header() {
     return (
-        <header>
-            <div className="btn-menu">
-                <span></span>
-                <span></span>
-                <span></span>   
+        <>
+            <header>
+                <div className="btn-menu">
+                    <span></span>
+                    <span></span>
+                    <span></span>   
+                </div>
+                <h1><Link to="/"><img src="/logo.svg" alt="logo"/></Link></h1>
+                <Link to="/profile"><div className="foto-perfil"></div></Link>
+            </header>
+
+            <div className="barra-pesquisa">
+                <input type="text" className="pesquisa-input" />
             </div>
-            <h1><Link to="/"><img src="/logo.svg" alt="logo"/></Link></h1>
-            <Link to="/profile"><div className="foto-perfil"></div></Link>
-        </header>
+        </>
     )
 }
