4471515230dcb9068e9c5480a6da3099614c2908
pedro <pedrosantaella61@gmail.com>
2025-10-01 21:39:04 -0300

    Criando estrutura do header

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom"
+
+export default function Header() {
+    return (
+        <header>
+            <div className="btn-menu">
+                <span></span>
+                <span></span>
+                <span></span>   
+            </div>
+            <h1><Link to="/"><img src="/logo.svg" alt="logo"/></Link></h1>
+            <Link to="/profile"><div className="foto-perfil"></div></Link>
+        </header>
+    )
+}
